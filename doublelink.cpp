6d7f468c92febaef7d291af110d09e44932dcaa0
NurvyyMelon <adinnaufal9@gmail.com>
2026-04-30 18:57:05 +0700

    Tambahkan definisi kelas untuk Node dan DoubleLinkedList di doublelink.cpp

--- doublelink.cpp
+++ doublelink.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Node
+{
+public:
+    int noMhs;
+    Node *next;
+    Node *prev;
+};
+
+class DoubleLinkedList
+{
+private:
+    Node *START;
